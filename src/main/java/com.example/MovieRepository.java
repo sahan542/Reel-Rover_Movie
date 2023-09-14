765f4598714fc6c33ced3e27ac4516cafdbca1ce
sahan542 <sahanrashmika542@gmail.com>
2023-09-14 17:14:33 +0530

    Connect to mongo DB

---
+++ src/main/java/com.example/MovieRepository.java
@@ -0,0 +1,11 @@
+package com.example;
+
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
+}
