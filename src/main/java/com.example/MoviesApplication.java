c105db389a765999b1660e46b62dd9c32d8d057d
sahan542 <sahanrashmika542@gmail.com>
2024-02-16 00:38:51 +0530

    restart project

--- src/main/java/com.example/MoviesApplication.java
+++ src/main/java/com.example/MoviesApplication.java
@@ -12,14 +12,6 @@ public class MoviesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
-/*
-	@GetMapping("/root")
-	public String apiRoot(){
-		return "HAPPY BIRTHDAY!";
-
-
-	}
-	*/
 
 
 }
--- src/main/java/com.example/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-
-public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
-}
