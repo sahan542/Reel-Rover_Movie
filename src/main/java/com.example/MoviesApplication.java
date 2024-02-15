1d371784bee2530abdfb187ea70a342a9e00f1c6
sahan542 <sahanrashmika542@gmail.com>
2024-02-16 02:38:28 +0530

    env files added

--- src/main/java/com.example/MoviesApplication.java
+++ src/main/java/com.example/MoviesApplication.java
@@ -2,16 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
-
-
 }
