37e1deae70e0906887f697a3c276fa51a01be3f0
sahan542 <sahanrashmika542@gmail.com>
2024-02-18 00:21:03 +0530

    Carousal Added

---
+++ Frontend/front/src/hero/Hero.jsx
@@ -0,0 +1,39 @@
+import './Hero.css';
+//import { Carousel } from 'react-bootstrap';
+import Carousel from 'react-material-ui-carousel';
+import { Paper } from '@mui/material';
+import React from 'react';
+
+const Hero = ({ movies }) => {
+ 
+  return (
+    <div>
+      <Carousel>
+      {
+            movies?.map((movie) =>{
+                return(
+                    <Paper key={movie.imdbId}>
+                        <div className = 'movie-card-container'>
+                            <div className="movie-card" style={{"--img": `url(${movie.backdrops[0]})`}}>
+                                <div className="movie-detail">
+                                    <div className="movie-poster">
+                                        <img src={movie.poster} alt="" />
+                                    </div>
+                                    <div className="movie-title">
+                                        <h4>{movie.title}</h4>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </Paper>
+                )
+            })
+        }
+
+      </Carousel>
+
+    </div>
+  );
+};
+export default Hero;
