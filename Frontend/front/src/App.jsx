b42df0ea14b686215c669e545d1c2dfcc8ab0162
sahan542 <sahanrashmika542@gmail.com>
2024-02-18 01:09:53 +0530

    Navbar added

--- Frontend/front/src/App.jsx
+++ Frontend/front/src/App.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 import Layout from './components/Layout';
 import {Routes,Route } from 'react-router-dom';
 import Home from './components/home/Home';
+import Header from './components/header/Header';
 
 const App = () => {
   const [movies, setMovies] = useState();
@@ -26,6 +27,7 @@ const App = () => {
 
   return (
     <div className='App'>
+      <Header/>
       <Routes>
         <Route path="/" element={<Layout/>}>
               <Route path="/" element={<Home movies={movies} />}></Route>
